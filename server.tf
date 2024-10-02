2fd58e60ef31ed3cb76edc7387f6d79b1d52aca8
Malcolm Soto <51336220+malcolmso@users.noreply.github.com>
2024-10-01 20:29:29 -0500

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,28 +1,29 @@
 resource "proxmox_vm_qemu" "server" {
-name = "server04"
-desc = "Ubuntu Server"
-vmid = "200"
-target_node = "proxmox01"
+  name        = "server04"
+  desc        = "Ubuntu Server"
+  vmid        = "200"
+  target_node = "proxmox01"
+  agent       = 1
 
-agent = 1
+  # Enter the clone template info
+  clone       = "server03"
+  cores       = 2
+  sockets     = 1
+  cpu         = "host"
+  memory      = 2048
 
-#enter the clone template info
+  # Network configuration
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"  # Corrected from "VirtO (paravirtualized)"
+  }
 
-clone = "server03"
-cores = 2
-sockets = 1
-cpu = "host"
-memory = 2048
-
-network {
-  bridge = "vmbr0"
-  model = "VirtO (paravirtualized)"
-}
-
-#because cloning. the disk statements need to match
-disk {
-  storage = "local-lvm"
-  type = "SCSI"
-  size = "60G"
-}
+  # Disk configuration
+  disk {
+    size         = "60G"
+    type         = "scsi"
+    storage      = "local-lvm"
+    #storage_type = "lvm"  # gives error
+    iothread     = 1       # Use 1 instead of true for the number
+  }
 }
